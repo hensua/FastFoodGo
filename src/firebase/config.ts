685e5ad5fa5af9e7229766432d6573428b09f60f
Henry Arias <arias2003henri@gmail.com>
2025-12-12 16:36:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3968442548-817f2",
+  "appId": "1:522971243914:web:6ef3da3ff50c08b9619473",
+  "apiKey": "AIzaSyCYXk4Olr80eJCuxI4JOXY14MY83GcYsxE",
+  "authDomain": "studio-3968442548-817f2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "522971243914"
+};
