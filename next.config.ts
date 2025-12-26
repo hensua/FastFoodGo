21dbf68e4f3d2c74524de34ffff7fa952b5e8ae1
Henry Arias <arias2003henri@gmail.com>
2025-12-26 04:59:52 +0000

    ## Error Type Runtime Error
    ## Error Message
    Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'farmanorte.com.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
